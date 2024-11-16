6918423753459cd54e709c9fe3edb3e977e3e9b7
Yordan Bahchevanov <yordanbah9376@gmail.com>
2024-11-16 12:31:01 +0200

    feat: Linking the app to postgres database

--- designSpace/settings.py
+++ designSpace/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'designSpace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'design_space_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Dancho_SQL_db',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -117,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
